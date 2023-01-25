13b79220363186d978679859a12fb5813a9f94bb
Khahory <angeld05@live.com>
2023-01-24 20:27:23 -0400

    funciona async en test

--- tests/test_pokemon/test_save.py
+++ tests/test_pokemon/test_save.py
@@ -1,12 +1,17 @@
 from models import PokemonModel
+import pytest
+
+# indicamos que vamos a usar el plugin pytest_asyncio
+# con esto puedo llamar funciones async
+pytest_plugins = ('pytest_asyncio',)
 
 
-def test_save_pokemon():
+@pytest.mark.asyncio
+async def test_save_pokemon():
     # creamos un pokemon
     pokemon_name = "Pikachu"
     pokemon_tipo = "Electrico"
 
     # guardamos el pokemon
-    res = PokemonModel.save_pokemon(pokemon_name, pokemon_tipo)
+    res = await PokemonModel.save_pokemon(pokemon_name, pokemon_tipo)
     assert res is True
-
